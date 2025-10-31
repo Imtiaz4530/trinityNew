d9740b4b63bb5448d7ef65b2b6dc71b5c60cd832
Imtiaz4530 <apual4016@gmail.com>
2025-10-31 21:12:39 +0600

    correct spelling mistake

--- server/controllers/story.controller.js
+++ server/controllers/story.controller.js
@@ -1,4 +1,4 @@
-// const Story = require('../models/Story.model');
+const Story = require('../models/story.model');
 
 // const createStory = async (req, res) => {
 //     const { title, content, link } = req.body;
