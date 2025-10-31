9eb0c7cdf005b2215dac7f121a4fd2f2a5120902
Imtiaz4530 <apual4016@gmail.com>
2025-10-31 20:52:43 +0600

    add story routes

---
+++ server/routes/story.routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const {createStory, addStory, getAllStories} = require('../controllers/story.controller.js');
+
+router.get('/', getAllStories);
+router.post('/create', createStory);
+router.post('/add', addStory);
+
+module.exports = router;
