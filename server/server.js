8e804e42b1ff1e5146b5db31dec53eec28e5f2e4
Imtiaz4530 <apual4016@gmail.com>
2025-10-31 20:29:50 +0600

    cors setup

--- server/server.js
+++ server/server.js
@@ -4,7 +4,10 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL, 
+  credentials: true,
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
